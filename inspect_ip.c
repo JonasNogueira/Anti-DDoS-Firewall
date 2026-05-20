2265a687bc42874ffe90b4a9a22906f53c39ca4b
JonasNogueira <jonas.nogdsilva@gmail.com>
2026-05-20 02:14:26 -0300

    acessar ip

---
+++ inspect_ip.c
@@ -0,0 +1,32 @@
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/if_ether.h> 
+#include <linux/ip.h>       
+#include <linux/in.h>       
+
+SEC("xdp")
+int inspect_packet(struct xdp_md *ctx)
+{
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+
+    struct ethhdr *eth = data;
+
+    if ((void *)(eth + 1) > data_end)
+        return XDP_PASS;
+
+    if (eth->h_proto != __constant_htons(ETH_P_IP))
+        return XDP_PASS;
+
+    struct iphdr *iph = (void *)(eth + 1);
+
+    if ((void *)(iph + 1) > data_end)
+        return XDP_PASS;
+
+    unsigned int src_ip = iph->saddr;
+    bpf_printk("Pacote recebido de: %pI4\n", &src_ip);
+
+    return XDP_PASS;
+}
+
+char LICENSE[] SEC("license") = "GPL";
